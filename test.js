cd8d42acb91ea7a5dc239fcb97eb6f7c21fbb11b
Mario Lazzari <mario.lazzari@gmail.com>
2020-06-04 21:26:30 +0200

    Done lesson 3 chapter 3

--- test.js
+++ test.js
@@ -21,27 +21,48 @@ const student = new mongoose.Schema(
         type: Number,
       },
     },
+    school: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "School",
+      required: true,
+    },
   },
+
   { timestamps: true }
 );
 
+const school = new mongoose.Schema({
+  name: String,
+  eopenSince: Number,
+  students: Number,
+  isGreat: true,
+});
+
 // model
-const Student = mongoose.model("student", student);
+const School = mongoose.model("School", school);
+const Student = mongoose.model("Student", student);
 
 // db connection
 connect()
   .then(async (conn) => {
     // insert
-    const student = await Student.create({ firstName: "Mario" });
-    console.log(student);
+    //let student = await Student.create({ firstName: "Mario" });
+    //console.log(student);
     // find
-    const student = await Student.find({ firstName: "test" });
-    const found = await Student.findById("xxxx");
-    const deleted = await Student.findByIdAndDelete("xxxx");
-    const updated = await Student.findByIdAndUpdate("xxxx", {});
+    //let student = await Student.find({ firstName: "test" });
+    //const found = await Student.findById("xxxx");
+    //const deleted = await Student.findByIdAndDelete("xxxx");
+    //const updated = await Student.findByIdAndUpdate("xxxx", {});
+
+    // populaet
+    const school = await School.create({ name: "My school" });
+    const student = await Student.create({
+      firstName: "Gino",
+      school: school._id,
+    });
+    const match = await Student.findById(student._id).populate("school").exec();
+    console.log(match);
   })
   .catch((ex) => {
     console.error(ex);
   });
-
-// insert
